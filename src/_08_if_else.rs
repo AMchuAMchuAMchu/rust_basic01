cbadb71387972f3f13fd21ef5b2ae4acb0365bbc
AMchuAMchuAMchu <2466130840@qq.com>
2022-09-12 10:44:35 +0800

    AM 01st commit

---
+++ src/_08_if_else.rs
@@ -0,0 +1,27 @@
+/**
+*Description==>TODO  
+*BelongsProject==>rust_basic01  
+*BelongsPackage==>  
+*CreateTime==>2022-09-09 13:55:35  
+*Version==>1.0  
+*Author==>02雪乃赤瞳楪祈校条祭制作委员会 wyq_start  
+*/
+
+fn main() {
+
+    let flag = 6;
+
+    if flag > 30 {
+        println!("nice!nice!!");
+    } else {
+        println!("起飞~~...^_^");
+    }
+
+    let args = false;
+
+    let result = if args { "666" } else { "西内~~^_^" };
+
+    println!("{} >>", result);
+
+
+}
--- src/main.rs
+++ src/main.rs
@@ -2,26 +2,18 @@
 *Description==>TODO  
 *BelongsProject==>rust_basic01  
 *BelongsPackage==>  
-*CreateTime==>2022-09-09 13:55:35  
+*CreateTime==>2022-09-12 10:37:34  
 *Version==>1.0  
 *Author==>02雪乃赤瞳楪祈校条祭制作委员会 wyq_start  
 */
 
 fn main() {
 
-    let flag = 6;
+    let x = String::new();
+    let y = x;
+    println!("{},{}",x,y);
 
-    if flag > 30 {
-        println!("nice!nice!!");
-    } else {
-        println!("起飞~~...^_^");
-    }
 
-    let args = false;
-
-    let result = if args { "666" } else { "西内~~^_^" };
-
-    println!("{} >>", result);
 
 
 }
