bb705b08dade6389c5c2b7e75334517c7ac38a62
AMchuAMchuAMchu <2466130840@qq.com>
2022-09-15 21:27:11 +0800

    AM 01st commit

--- src/_14_borrowTest01.rs
+++ src/_14_borrowTest01.rs
@@ -2,20 +2,23 @@
 *Description==>TODO  
 *BelongsProject==>rust_basic01  
 *BelongsPackage==>  
-*CreateTime==>2022-09-15 21:24:09  
+*CreateTime==>2022-09-15 21:19:10  
 *Version==>1.0  
 *Author==>02雪乃赤瞳楪祈校条祭制作委员会 wyq_start  
 */
 
 fn main(){
 
-    let mut s1 = String::from("999");
+    let mut s = String::from("bye..bye...");
 
-    let s12 = &s1;
-    let s13 = &s1;
-    let s14  =&mut s1;
+    {
+        let s1 = &mut s;
+    }
 
-    println!("{},{},{},{}",s1,s12,s13,s14);
+    let s2 = &mut s;
+
+    println!("{}",s2);
+
 
 
 }
