18d9e1bb558930a84d98ac98fb36698bef8ebf88
AMchuAMchuAMchu <2466130840@qq.com>
2022-09-18 15:23:44 +0800

    AM 01st commit

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::time::SystemTime;
 
+#[derive(Debug)]
 /**
 *Description==>TODO  
 *BelongsProject==>rust_basic01  
@@ -8,8 +9,22 @@ use std::time::SystemTime;
 *Version==>1.0  
 *Author==>02雪乃赤瞳楪祈校条祭制作委员会 wyq_start  
 */
+struct Rectangle {
+    width: i32,
+    height: i32,
+}
 
-fn main(){
+fn main() {
+    let rect01 = Rectangle {
+        width: 10,
+        height: 20,
+    };
 
+    let res01 = rect_area(&rect01);
 
+    println!("结果: {};矩形的信息:{:#?}", res01, rect01);
+}
+
+fn rect_area(rect: &Rectangle) -> i32 {
+    return rect.height * rect.width;
 }
