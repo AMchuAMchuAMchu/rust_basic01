a48c57d18601e97d7b83cc44457ad878943b6ca5
AMchuAMchuAMchu <2466130840@qq.com>
2022-09-21 19:12:26 +0800

    AM 01st commit

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,32 @@
 *Version==>1.0  
 *Author==>02雪乃赤瞳楪祈校条祭制作委员会 wyq_start  
 */
+#[derive(Debug)]
 
 
+
+struct Anime{
+    animeName:i32,
+    time:i32
+}
+
+impl Anime {
+    fn sayHello(&self)->i32{
+        return self.animeName+self.time;
+    }
+}
+
+fn main(){
+
+    let a01 = Anime{
+        animeName:36,
+        time:2022,
+    };
+
+    println!("01 {}",a01.sayHello());
+
+    println!("02 {:#?}",a01);
+
+}
 
 
